b81ef03a3ebfc7e6711ec6d7ee93012772a029fd
nghilq <nghilq@deha-soft.com>
2022-06-21 16:41:44 +0700

    remove unused packages

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from './common/prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({ origin: '*' });
-  app.use(helmet());
   const config = app.get(ConfigService);
   app.setGlobalPrefix(config.get('API_PREFIX'));
   app.useStaticAssets(join(__dirname, `..${config.get('STATIC_ASSETS_PATH')}`));
